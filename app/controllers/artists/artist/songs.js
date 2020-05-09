74a75fd12b27707904b17f65d99f4373a438065d
Lenora Porter <lporter630@gmail.com>
2020-05-09 19:25:52 -0400

    Add songs

---
+++ app/controllers/artists/artist/songs.js
@@ -0,0 +1,29 @@
+import Controller from '@ember/controller';
+import Song from 'music-collection/models/song';
+import { action } from '@ember/object';
+import { empty } from '@ember/object/computed';
+
+export default class SongsController extends Controller {
+  isAddingSong = false 
+  newSongTitle = ""
+
+  @empty('newSongTitle') isAddButtonDisabled
+
+  @action
+  addSong() { 
+    this.set('isAddingSong', true);
+  }
+
+  @action
+  cancelAddSong() { 
+    this.set('isAddingSong', false);
+  }
+
+  @action
+  saveSong(event) {
+    event.preventDefault();
+    let newSong = Song.create({ title: this.newSongTitle }); 
+    this.model.songs.pushObject(newSong); 
+    this.set('newSongTitle', '');
+  } 
+}
