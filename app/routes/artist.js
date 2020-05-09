40b90b8af50f945e172bee094d17a55413b8622c
Lenora Porter <lporter630@gmail.com>
2020-05-09 07:30:25 -0400

    Update Artist controller with list of Artist

--- app/routes/artist.js
+++ app/routes/artist.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+import { A } from '@ember/array';
+
+const Artist = EmberObject.extend({
+  name: '',
+});
 
 export default class ArtistRoute extends Route {
+  model() {
+    let megStallion = Artist.create({ name: 'Megan Thee Stallion' });
+    let cardiB = Artist.create({ name: 'Cardi B '});
+    let dojaCat = Artist.create({ name: 'Doja Cat'});
+
+    return A([megStallion, cardiB, dojaCat]);
+  }
 }
