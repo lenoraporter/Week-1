0ffd0cd2d714112025ed272cd347301f0da647f2
Lenora Porter <lporter630@gmail.com>
2020-05-21 08:22:14 -0400

    Give each url a browser title

--- app/router.js
+++ app/router.js
@@ -4,6 +4,38 @@ import config from './config/environment';
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
+
+  init() {
+    this._super();
+    this.setDocumentTitle();
+  },
+
+  setDocumentTitle() {
+    this.on('routeDidChange', (transition) => {
+      if (!transition.to) {
+        return;
+      }
+      let toRouteName = transition.to.name;
+      let pageTitles = {
+        'artists.index': () => {
+          return 'Artists';
+        },
+        'artists.artist.songs': () => {
+          let artistRouteInfo = transition.to.find(info => info.name.includes('artists.artist'));
+          let artistSlug = artistRouteInfo.params.slug;
+          let artistName = artistSlug.split('-').map(s => capitalize(s)).join('');
+          return `${bandName} songs`;
+        }
+      }
+      let titleSegment = [];
+      let titleSetter = pageTitles[toRouteName];
+      if (titleSetter) {
+        titleSegments.push(titleSetter());
+      }
+      titleSegments.push('Rock and Roll with Ember.js');
+      document.title = titleSegments.join(' - ');
+    });
+  }
 }
 
 Router.map(function() {
